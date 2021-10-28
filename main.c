de7ea6c98dd276223618afc0609af997bb512480
Pedro Arroz <p5403@ulusofona.pt>
2021-10-28 08:30:07 +0000

    corrigido bug do numero de argumentos e adicionado abertura de ficheiros

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) 
 {
-	if (argc < 3)
+	FILE *fp;
+	if (argc < 4)
 	{
 		puts("Syntax error");
 		return 0;
@@ -11,6 +13,21 @@ int main(int argc, char *argv[])
 	puts(argv[1]);
 	puts(argv[2]);
 	puts(argv[3]);
+
+
+	if (strcmp(argv[2], "r") == 0)
+		fp = fopen(argv[1], "r");
+	else
+		fp = fopen(argv[1], "rb");
+
+
+	if (fp == NULL)
+	{
+		puts("ERRO ao abrir o ficheiro");
+		return 0;
+	}
+
+	fclose(fp);
 
 	return 0;
 }
